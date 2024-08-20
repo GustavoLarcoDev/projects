51f4d3965ba85ebf401bd599d7f745fd1b8ba394
Gustavo Larco <gustavo.larcoj@gmail.com>
2024-08-20 18:18:46 -0500

    Feature 13 complete

--- projects/views.py
+++ projects/views.py
@@ -7,3 +7,9 @@ from django.contrib.auth.decorators import login_required
 def list_projects(request):
     projects = Project.objects.filter(owner=request.user)
     return render(request, 'projects/ProjectsList.html', {'projects': projects})
+
+
+@login_required
+def show_project(request, id):
+    detail = Project.objects.get(id=id)
+    return render(request, 'projects/DetailProject.html', {'detail': detail })
--- tasks/admin.py
+++ tasks/admin.py
@@ -3,4 +3,3 @@ from tasks.models import Task
 
 
 admin.site.register(Task)
-
